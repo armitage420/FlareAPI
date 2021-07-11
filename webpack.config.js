afd24be2f141a74c3cd11c3e6d9fc0250cb0e4f9
Rayeka <araikasingh@gmail.com>
2021-07-11 09:24:31 +0530

    ✨ added content-type and custom parameter box

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+let htmlPageNames = [];
+let multipleHtmlPlugins = htmlPageNames.map((name) => {
+  return new HtmlWebpackPlugin({
+    template: `./src/pages/${name}.html`, // relative path to the HTML files
+    filename: `${name}.html`, // output HTML files
+    chunks: ["main", `${name}`], // respective JS files
+  });
+});
+
 module.exports = {
   entry: "./src/js/index.js",
   devtool: "eval-cheap-source-map",
@@ -13,10 +22,40 @@ module.exports = {
       template: "./index.html",
       chunks: ["main"],
     }),
-  ],
+  ].concat(multipleHtmlPlugins),
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(svg|gif|png|eot|woff|ttf)$/,
+        use: ["url-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(mp4|png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
   },
 };

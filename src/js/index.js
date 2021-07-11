969df83cad74b1f7d1dcf0c036ee75afc45f2eaa
Rayeka <araikasingh@gmail.com>
2021-07-11 11:57:05 +0530

    💄 show correct content type for selection

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 import "bootstrap/dist/css/bootstrap.css";
+console.log("working");
+document.getElementById("customParam").style.display = "none";
+
+document.getElementById("JSON").addEventListener("click", () => {
+  document.getElementById("customParam").style.display = "none";
+  document.getElementById("jsonRequest").style.display = "block";
+});
+
+document.getElementById("customParameters").addEventListener("click", () => {
+  document.getElementById("customParam").style.display = "block";
+  document.getElementById("jsonRequest").style.display = "none";
+});
